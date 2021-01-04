0a036146c8bdf6b194416c3c666941b3589a25d3
Arghadeep <archakraborty@teksystems.com>
2021-01-04 22:44:52 +0530

    Home Screen First Commit

---
+++ Swoosh/Swoosh/CustomButton.swift
@@ -0,0 +1,18 @@
+//
+//  CustomButton.swift
+//  Swoosh
+//
+//  Created by Arghadeep  on 04/01/21.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 3.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+
+}
