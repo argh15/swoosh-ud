5483b0e7741fc0588243273b5fb036177f5c2956
Arghadeep <archakraborty@teksystems.com>
2021-01-11 15:45:07 +0530

    UI Screens. Connection through storyboard segues and programmatic segues.

--- Swoosh/Swoosh/Controller/SkillVC.swift
+++ Swoosh/Swoosh/Controller/SkillVC.swift
@@ -1,19 +1,17 @@
 //
-//  ViewController.swift
+//  SkillVC.swift
 //  Swoosh
 //
-//  Created by Arghadeep  on 09/11/20.
+//  Created by Arghadeep  on 05/01/21.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class SkillVC: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
