2601ed78fd5eed3deb5461070789c52568e6eac1
argh15 <argho.chakr@gmail.com>
2021-01-11 21:37:59 +0530

    Feat i os league screen (#3)
    * Setting League Type and button enabling.
    
    * Changes in SkillVC wrt showing the final result.
    Changes in Enum
    
    Co-authored-by: Arghadeep <archakraborty@teksystems.com>

---
+++ Swoosh/Swoosh/Utils/Enum.swift
@@ -0,0 +1,23 @@
+//
+//  Enum.swift
+//  Swoosh
+//
+//  Created by Arghadeep  on 11/01/21.
+//
+
+import Foundation
+
+enum LeagueType: String{
+    case mens = "mens",
+         womens = "womens",
+         coed = "coed",
+         none = ""
+}
+
+enum SkillType: String{
+    case beginner = "beginner",
+         baller = "baller",
+         none = ""
+}
+
+
